7e4b521ae8eb1c5a306e30fb82dfe465c617aedd
ExBlue234 <paul.v.tran@uconn.edu>
2025-04-08 03:33:49 +0000

    Coin problem

--- Coding Projects/LargestSum.py
+++ Coding Projects/LargestSum.py
@@ -1,6 +1,6 @@
 # Main Function below
 
-def Largest_Sum(triangle_list):
+def Largest_Sum(triangle_list:list[list[int]]): # Expects a triangle like format below
     return helper_Largest_Sum(i=0, j=0, cache={}, num_list=triangle_list)
 
 def helper_Largest_Sum(i, j, cache, num_list):
@@ -9,12 +9,12 @@ def helper_Largest_Sum(i, j, cache, num_list):
     if (i, j) in cache:
         return cache[(i, j)] # precomputated result
     
-    down_left = helper_Largest_Sum(i+1, j, cache, num_list)
-    down_right = helper_Largest_Sum(i+1, j+1, cache, num_list)
+    down_left = helper_Largest_Sum(i+1, j, cache, num_list) # Left recursion
+    down_right = helper_Largest_Sum(i+1, j+1, cache, num_list) # Right recursion
     
-    cache[(i, j)] = num_list[i][j] + max(down_left, down_right)
+    cache[(i, j)] = num_list[i][j] + max(down_left, down_right) # Goes down the adjacent path that has the greatest value
     
-    return cache[(i, j)]
+    return cache[(i, j)] # Returns current result
 
 if __name__ == "__main__":
     triangle = [
@@ -23,5 +23,6 @@ if __name__ == "__main__":
     [1, 2, 7],
     [8, 5, 9, 3]
     ]
+    # Starting from 2, the top of this triangle
     
-    print(Largest_Sum(triangle))
+    print(Largest_Sum(triangle)) # 19 was the largest value produced after traversing adjacent paths (left or right) given any current location
